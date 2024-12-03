6e85178e89a2995a9c3c3c4af6aaa35ef3abe854
Celso de Oliveira Silva <celsoliveira@gmail.com>
2024-12-03 11:28:04 -0300

    Testando os verbos pelo postman

--- 03_RestWithASPNET_Person/Program.cs
+++ 03_RestWithASPNET_Person/Program.cs
@@ -1,12 +1,21 @@
+using _02_RestWithASPNET.Services;
+using _02_RestWithASPNET.Services.Implementations;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
 
+
+// Adicionando as services (Injeção de dependência)
+builder.Services.AddScoped<IPersonService, PersonServiceImplementation>();
+
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+
 
 app.UseHttpsRedirection();
 
